8c3a8e72a17d96fecb29f28be0d2c5d4917b207b
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-04-21 13:46:23 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello, world! First time using React</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
