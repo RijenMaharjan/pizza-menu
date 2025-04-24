29ffe15667ebf8520684c4696a891c12f38f4aab
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-04-24 16:02:06 +0545

    props

--- src/index.js
+++ src/index.js
@@ -100,6 +100,9 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) {
+    return null;
+  }
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -122,19 +125,34 @@ function Footer() {
   // if (hour >= openHour && hour <= closeHour) alert("We are open!");
   // else alert("Sorry we are closed!");
 
+  // if (!isOpen)
+  //   return (
+  //     <p>
+  //       We're happy to welcome you between {openHour}:00 to {closeHour}:00
+  //     </p>
+  //   );
+
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 to {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
